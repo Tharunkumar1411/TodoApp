e993de450eadb643bc17a7937de95ee19a0bc5ce
Tharunkumar001 <tharung1411@gmail.com>
2023-02-22 09:33:37 +0530

    bug fixing

--- src/pages/TodoPage.js
+++ src/pages/TodoPage.js
@@ -8,49 +8,8 @@ import ActiveTodo from "./components/ActiveTodo";
 import ExpiredTodo from "./components/ExpiredTodo";
 import { setActive } from "./store/active";
 import { Add } from "@mui/icons-material";
-import { makeStyles } from "@mui/styles";
 import { Button, Dialog, DialogTitle } from "@mui/material";
-
-const useStyles = makeStyles((theme) => ({
-    addBtn:{
-        display:"flex",
-        marginLeft:"auto",
-        marginRight:"auto",
-        color:"white",
-        fontWeight:"bold",
-        border:"solid 0.2rem white",
-        
-    },
-
-    dialogBody:{
-        padding:"1rem",
-        display:"flex",
-        flexDirection:"column",
-        gap:"0.5rem",
-        
-    },
-
-    inputField:{
-        background:"none",
-        padding:"0.2rem",
-        color:"black",
-        outline:"none",
-        borderRadius:"0.2rem",
-    },
-
-    switchTab:{
-        display:"flex",
-        flexDirection:"row",
-        gap:"1rem",
-        padding:"1rem",
-        justifyContent:"center"
-    },
-    swithchBtn:{
-        color:"white",
-        border:"0.1rem solid grey",
-    
-    }
-}));
+import styles from "../styles/Home.module.css";
 
 const notify = () => toast.success('Your Todo Added.');
 
@@ -60,7 +19,6 @@ const TodoPage = () => {
     const UserDetail = useSelector(getUser);
     const [form, setForm] = useState("");
     const [open, setOpen] = useState(false);
-    const classes = useStyles();
     const dispatch = useDispatch();
 
 
@@ -81,14 +39,14 @@ const TodoPage = () => {
         <div style={{paddingTop:"0.5rem"}}>
             <h1 style={{padding:"1rem"}}>Welcome {UserDetail}</h1>
 
-            <Button variant="outlined" endIcon={<Add />} className={classes.addBtn} onClick={() => setOpen(true)}>
-                Add Todo
+            <Button variant="outlined" className={styles.addBtn} onClick={() => setOpen(true)}>
+                Add Todo's
             </Button>
 
-            <div className={classes.switchTab}>
-                <Button className={classes.swithchBtn} onClick={() => setTab(<AllTodo />)}>All</Button>
-                <Button className={classes.swithchBtn} onClick={() => setTab(<ActiveTodo />)}>Active</Button>
-                <Button className={classes.swithchBtn} onClick={() => setTab(<ExpiredTodo />)}>Expired</Button>
+            <div className={styles.switchTab}>
+                <Button className={styles.swithchBtn} onClick={() => setTab(<AllTodo />)}>All</Button>
+                <Button className={styles.swithchBtn} onClick={() => setTab(<ActiveTodo />)}>Active</Button>
+                <Button className={styles.swithchBtn} onClick={() => setTab(<ExpiredTodo />)}>Expired</Button>
             </div>
 
             <div>
@@ -98,11 +56,11 @@ const TodoPage = () => {
             <div>
             <Dialog onClose={handleClose} open={open}>
                 <DialogTitle>Add Your Todo</DialogTitle>
-                    <form onSubmit={handleSubmit} className={classes.dialogBody}>
-                        <input type="text" placeholder='Todo' required className={classes.inputField} value={form} 
+                    <form onSubmit={handleSubmit} className={styles.dialogBody}>
+                        <input type="text" placeholder='Todo' required className={styles.inputField} value={form} 
                             onChange={(e) => setForm(e.target.value)}
                         />
-                        <button className={classes.btn} type="submit">Add</button>
+                        <button className={styles.btn} type="submit">Add</button>
                     </form>
                 </Dialog>
             </div>
